773c7c81de9e174c04b722d92a5e6fc02871f84c
ElectronicAPU <apurbanaskar3206@gmail.com>
2023-12-27 11:48:59 +0530

    mongo db connect

---
+++ modles/chatModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: {
+      type: String,
+      trim: true,
+    },
+    groupChat: {
+      type: Boolean,
+      default: false,
+    },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestMessage: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message",
+      },
+    ],
+    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
+
+const Chat = mongoose.Model("Chat", chatModel)
+
+module.exports = Chat;
+
+// chatName
+//isGroup
+//User
+//latestMessage
+//groupAdmin
