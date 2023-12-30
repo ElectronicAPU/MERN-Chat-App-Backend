6c34c3e96e73bf38f05f3a2ed1cef56f2ff11a8b
ElectronicAPU <apurbanaskar3206@gmail.com>
2023-12-30 19:55:58 +0530

    accessChat and fetchAllChats

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,48 +1,40 @@
 const asyncHandler = require("express-async-handler");
 const Chat = require("../modles/chatModel");
 const User = require("../modles/userModel");
+const Message = require("../modles/messageModel"); // Import the Message model
 
 const accessChat = asyncHandler(async (req, res) => {
-  //This is the ID which i am going to chats with
   const { userId } = req.body;
 
-  //IF not userId found
   if (!userId) {
     console.log("userId param not sent with request");
     return res.sendStatus(404);
   }
 
-  // if userId found then create a new chat
   var isChat = await Chat.find({
     isGroupChat: false,
-    //$all is used to match documents where the users array contains all the specified elements (req.user._id and userId).
     users: { $all: [req.user._id, userId] },
+    // ⬆️ req.user._id is the USER who actually logged in and userId is the one which logged in user want to chat with ****⬆️
   })
     .populate("users", "-password")
     .populate("latestMessage");
-  //⬆️ and here i populate the users from the chatModel without the password⬆️ and also added(populate) the latest message
 
-  //⬇️ here i gona populate the sender came from MESSAGE MODEL with name pic and email⬇️
   isChat = await User.populate(isChat, {
     path: "latestMessage.sender",
     select: "name , pic, email",
   });
-  // if chat is exist
+
   if (isChat.length > 0) {
-    res.sendStatus(isChat[0]);
+    res.status(200).send(isChat[0]); // Assuming isChat[0] is the intended result
   } else {
-    // if there are not any chat then i create a chat
     var chatData = {
       chatName: "sender",
       isGroupChat: false,
       users: [req.user._id, userId],
-      // ⬆️ here is we check the *req.user._id* who send the chat 1st and the reciver is userId⬆️
     };
 
     try {
-      // create the CHAT
       const createdChat = await Chat.create(chatData);
-      // This was the just created chat and find and sned it with out the passed
       const FullChat = await Chat.findOne({ _id: createdChat._id }).populate(
         "users",
         "-password"
@@ -56,4 +48,23 @@ const accessChat = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { accessChat };
+const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    const results = await Chat.find({ users: { $all: [req.user._id] } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 });
+
+    const populatedResults = await User.populate(results, {
+      path: "latestMessage.sender",
+      select: "name , pic, email",
+    });
+
+    res.status(200).send(populatedResults);
+  } catch (error) {
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
+module.exports = { accessChat, fetchChats };
